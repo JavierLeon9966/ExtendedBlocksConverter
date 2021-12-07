0c5cf51d4615365bde6b53c21c2493f50ee19b10
Covered123 <58715544+JavierLeon9966@users.noreply.github.com>
2021-12-07 19:27:28 -0300

    Plugin finished

---
+++ src/JavierLeon9966/ExtendedBlocksConverter/Main.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace JavierLeon9966\ExtendedBlocksConverter;
+
+use JavierLeon9966\ExtendedBlocksConverter\tile\Placeholder;
+
+use pocketmine\block\tile\TileFactory;
+use pocketmine\event\Listener;
+use pocketmine\event\world\ChunkLoadEvent;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener{
+	public function onLoad(): void{
+		TileFactory::getInstance()->register(Placeholder::class);
+	}
+
+	public function onEnable(): void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	/**
+	 * @priority MONITOR
+	 */
+	public function onChunkLoad(ChunkLoadEvent $event): void{
+		$world = $event->getWorld();
+		foreach($event->getChunk()->getTiles() as $tile){
+			if(!$tile instanceof Placeholder){
+				continue;
+			}
+			$pos = $tile->getPosition();
+			$world->setBlockAt($pos->x, $pos->y, $pos->z, $tile->getExtendedBlock(), false);
+			$tile->close();
+		}
+	}
+}
---
+++ src/JavierLeon9966/ExtendedBlocksConverter/tile/Placeholder.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace JavierLeon9966\ExtendedBlocksConverter\tile;
+
+use pocketmine\block\tile\Tile;
+use pocketmine\block\{BlockFactory, VanillaBlocks};
+use pocketmine\math\Vector3;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\world\World;
+
+class Placeholder extends Tile{
+	protected Block $block;
+
+	public function __construct(World $world, Vector3 $pos){
+		parent::__construct($world, $pos);
+		$this->block = VanillaBlocks::INFO_UPDATE();
+	}
+
+	public function readSaveData(CompoundTag $nbt): void{
+		$blockTag = $nbt->getCompoundTag('Block');
+		if($blockTag !== null){
+			$this->block = BlockFactory::getInstance()->get($blockTag->getShort('id'), $blockTag->getByte('meta'));
+		}
+	}
+
+	protected function writeSaveData(CompoundTag $nbt): void{
+		// NOOP
+	}
+
+	public function getExtendedBlock(): Block{
+		return clone $this->block;
+	}
+}
